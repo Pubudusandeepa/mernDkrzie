42a3cacd56fd8655809ae5f122edcc66e5da270c
Pubudusandeepa <ppsandeepa12@gmail.com>
2023-03-01 18:31:17 +0530

    comit

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -1,15 +1,15 @@
 const Footer = () => {
-	const link = "https://henok.us";
-	const target = "_blank";
+  const link = "https://henok.us";
+  const target = "_blank";
 
-	return (
-		<div className="container">
-			Copyright © <small>{new Date().getFullYear()}</small> Henok R. Bedassa{" "}
-			<a href={link} target={target}>
-				henok.us
-			</a>
-		</div>
-	);
+  return (
+    <div className="container">
+      Copyright © <small>{new Date().getFullYear()}</small> Mighty Shadow{" "}
+      <a href={link} target={target}>
+        henok.us
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
